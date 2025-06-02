9cb66e8c0013c2192e2be7921254417cee0403e2
Adyant <adyantajay@gmail.com>
2025-06-02 19:47:37 +0530

    Supabase Implemented

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,20 @@
+import CompanionForm from '@/components/CompanionForm'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const NewCompanion = () => {
+const NewCompanion = async() => {
+  const {userId} = await auth();
+  if(!userId) redirect('/sign-in');
+
   return (
-    <div>
-      New Companion
-    </div>
+    <main>
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Companion Builder</h1>
+
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import Cta from '@/components/Cta'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard 
+          id="123" name="ABC" topic="ABSTOPc" subject="ABCSub" duration={23} color="#ffda54"
+        />
+        <CompanionCard 
+          id="456" name="ABC" topic="ABSTOPc" subject="ABCSub" duration={23} color="#ffda54"
+        />
+        <CompanionCard 
+          id="789" name="ABC" topic="ABSTOPc" subject="ABCSub" duration={23} color="#ffda54"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionList title='AGDC' companions={recentSessions} classNames='w-2/3 max-lg:w-full' />
+        <Cta />
+      </section>
+    </main>
   )
 }
 
